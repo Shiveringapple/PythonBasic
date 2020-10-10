8475f1a36818eeb2320785ad2a8f46ca0c8611f7
Shiveringapple <69024425+Shiveringapple@users.noreply.github.com>
2020-10-10 23:27:00 +0800

    Add files via upload

---
+++ calculate/main2.py
@@ -0,0 +1,19 @@
+a = "Hello"
+print(a)
+
+a = a + "Python"
+print(a)
+
+# 同類型才能運算
+a = a + "3"
+print(a)
+
+print("長度:", len(a))
+
+print(a[5])
+print(a[len(a)-1])
+
+# 第二個數字不包含
+print(a[4:8])
+print(a[1:])
+
